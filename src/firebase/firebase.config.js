e69889b2135b5bb9ccfe2bddd0660f6d6f3c99f5
jasminaramim <jasminaramim2005@gmail.com>
2024-12-08 22:57:55 +0600

    env use

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,19 @@
 
+
 // Import the functions you need from the Firebase SDK
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your Firebase configuration
+// Your Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAqKBidrI63OnioZpQl34NhQ3_Wp8R1s-g",
-  authDomain: "crowd-cube-website.firebaseapp.com",
-  projectId: "crowd-cube-website",
-  storageBucket: "crowd-cube-website.firebasestorage.app",
-  messagingSenderId: "214979002710",
-  appId: "1:214979002710:web:c5cdb8fb0a3a4f4ed656f7"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectsID,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId ,
+  appId: import.meta.env.VITE_appId,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -27,4 +27,4 @@ export const db = getFirestore(app);
 export { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup };
 
 // Default export
-export default {app,db,auth};
+export default {app, db, auth};
